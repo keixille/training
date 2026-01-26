b06b61b4f7c903b830ddbe1e2578cecffc74d78d
Dian Aditya Nugraha <dian.nugraha@tech.jago.com>
2026-01-26 14:30:03 +0700

    feat: initial files hello_world_web

---
+++ flutter/hello_world_web/test/widget_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hello_world_web/main.dart'; // Import your app
+
+void main() {
+  // testWidgets('Should have MaterialApp with title: Hello World Web', (WidgetTester tester) async {
+  //   // Given
+  //   // When
+  //   await tester.pumpWidget(MyApp());
+  //
+  //   // Then
+  //   final MaterialApp app = tester.widget(find.byType(MaterialApp));
+  //   expect(app.title, equals('Hello World Web'));
+  // });
+
+  testWidgets('Should have AppBar with title: Hello World Web App', (WidgetTester tester) async {
+    // Given
+    // When
+    await tester.pumpWidget(MyApp());
+
+    // Then
+    final appBarText = find.descendant(
+      of: find.byType(AppBar),
+      matching: find.text('Hello World Web App'),
+    );
+    expect(appBarText, findsOneWidget);
+  });
+
+  // testWidgets('Should have center text: Hello World', (WidgetTester tester) async {
+  //   await tester.pumpWidget(MyApp());
+  //
+  //   expect(find.byType(Center), findsOneWidget);
+  //   expect(find.text('Hello World'), findsOneWidget);
+  // });
+}
